09064d686d270e9b785b29e9e281cade0ab7a890
j2.jeremias <j2.jeremias@gmail.com>
2024-10-30 15:33:54 +1000

    change questions

--- src/App.vue
+++ src/App.vue
@@ -72,7 +72,7 @@ export default {
       this.question = undefined;
 
       this.axios
-        .get('https://opentdb.com/api.php?amount=1&category=18')
+        .get('https://opentdb.com/api.php?amount=1&category=26')
         .then((response) => {
           this.question = response.data.results[0].question;
           this.incorrectAnswers = response.data.results[0].incorrect_answers;
